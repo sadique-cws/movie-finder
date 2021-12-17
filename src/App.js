4c704e41fa17dbcb9012f1eb69d14f090745c41f
sadique-cws <sadique.hussain96@gmail.com>
2021-12-17 08:12:02 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,118 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer,useEffect } from 'react';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const API_KEY = "16c66b0f7fd3c3447e7067ff07db3197";
+  // https://api.themoviedb.org/3/search/
+
+  useEffect(() => {
+    if(state.submittedMovieTitle){
+      const fetchData = async () => {
+        dispatch({type: "FETCH_DATA"});
+        try{
+          const result = await axios(
+            `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${state.submittedMovieTitle}`
+          );
+          dispatch({
+            type: ACTIONS.FETCH_DATA_SUCCESS,
+            value: result.data.results,
+          });
+          
+        } catch (error){
+          dispatch({type: "FETCH_FAILURE"})
+        }
+      };
+       fetchData();
+    }
+  },[state.submittedMovieTitle]);
+
+  const initialState = {
+    typedInMovieTitle : "",
+    submittedMovieTitle: "",
+    movies : [],
+    isLoading: false,
+    isError : false,
+  }
+
+  const ACTIONS  = {
+    TYPE_SEARCH : "TYPE_SEARCH",
+    SUBMIT_SEARCH : "SUBMIT_SEARCH",
+    FETCH_DATA : "FETCH_DATA",
+    FETCH_DATA_SUCCESS: "FETCH_DATA_SUCCESS",
+    FETCH_DATA_FAIL : "FETCH_DATA_FAIL"
+  }
+
+  function onChange(event){
+      dispatch({
+          type: ACTIONS.TYPE_SEARCH,
+          value : event.target.value,
+      });
+  }
+
+  const reducer = (state, action) => {
+      switch(action.type){
+        case ACTIONS.TYPE_SEARCH:
+          return {
+            ...state,
+            typedInMovieTitle:action.value
+          };
+        
+        case ACTIONS.SUBMIT_SEARCH:
+          return {
+            ...state,
+            submittedMovieTitle : state.typedInMovieTitle
+          };
+        
+        case ACTIONS.FETCH_DATA:
+          return {
+            ...state,
+            isLoading:true,
+          }
+          
+        case ACTIONS.FETCH_DATA_SUCCESS:
+          return {
+            ...state,
+            movies: action.value,
+            isLoading: false,
+          }
+        
+        case ACTIONS.FETCH_DATA_FAIL:
+          return {
+            ...state,
+            isError: true
+          }
+          default:
+            return state;
+      }
+  }
+
+  function onSubmit(event){
+      event.preventDefault();
+
+      dispatch({
+        type: ACTIONS.SUBMIT_SEARCH
+      });
+
+      console.log(state.movies)
+
+  }
+    return (
+    <div className="container-fluid py-5">
+      <header className="text-center">
+        <h2 className="display-6 text-white">Movie <span className="text-danger">Finder</span></h2>
       </header>
+
+     <div className="container">
+        <div className="row">
+            <div className="col-3 mx-auto">
+                <form onSubmit={onSubmit}>
+                  <input type="text" className="form-control border-0 shadow-none" placeholder="Type movie title" onChange={onChange}/>
+              </form>
+            </div>
+        </div>
+     </div>
     </div>
   );
 }
